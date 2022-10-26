abb781137228f227078deefdfe2b8a75e19d5cad
henriquemaycon <mayconmarquesh@gmail.com>
2022-10-26 19:55:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Calculadora de IMC
+
+Projeto em React + Typescript desenvolvendo uma calculadora de IMC.
+
+Projeto Desenvolvido durante o curso [B7 Web](https://b7web.com.br/fullstack)
+
+### Instalação
+
+- `npm install`
+
+### Para rodar
+
+- `npm run dev`
---
+++ src/App.tsx
@@ -0,0 +1,106 @@
+import { useState } from 'react';
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png';
+import leftArrowImage from './assets/leftarrow.png';
+import { GridItem } from './components/GridItem';
+import {levels, calculateImc, Level} from './helpers/imc';
+
+function App() {
+
+	const [heightField, setHeightField] = useState<number>(0);
+	const [weightField, setWeightField] = useState<number>(0);
+   const [toShow, setToShow] = useState<Level | null>(null);
+
+	function handleCalculateButton() {
+
+		if(heightField && weightField) {
+
+			setToShow(calculateImc(heightField, weightField))
+
+		} else {
+
+			alert('Digite todos os campos')
+
+		}
+	}
+
+   function handleBackButton() {
+      setToShow(null);
+      setHeightField(0);
+      setWeightField(0);
+   }
+
+	return (
+		<div className={styles.main}>
+
+			<header>
+
+				<div className={styles.headerContainer}>
+					<img src={poweredImage} alt="" width={150}/>
+				</div>
+
+			</header>
+
+			<div className={styles.container}>
+
+				<div className={styles.leftSide}>
+
+					<h1>Calcule o seu IMC.</h1>               
+					<p>
+                  IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial da Saúde para calcular o peso ideal de cada pessoa.
+               </p>
+
+					<input 
+						type="number"
+						placeholder='Digite a sua altura em metros'
+						value={heightField > 0 ? heightField : ''}
+						onChange={e => setHeightField(+e.target.value)}
+                  disabled={toShow ? true : false}
+					/>
+
+					<input 
+						type="number"
+						placeholder='Digite o seu peso em kilos'
+						value={weightField > 0 ? weightField : ''}
+						onChange={e => setWeightField(+e.target.value)}
+                  disabled={toShow ? true : false}
+					/>
+
+					<button onClick={handleCalculateButton} disabled={toShow ? true : false}>Calcular</button>
+
+				</div>
+
+				<div className={styles.rightSide}>
+               
+               {!toShow && 
+                  <div className={styles.grid}>
+						
+                     {levels.map((item, key) => (
+
+                        <GridItem key={key} item={item}/>
+
+                     ))}
+						
+					   </div>
+               }
+
+					{toShow && 
+                  <div className={styles.rightBig}>
+
+                     <div className={styles.rightArrow} onClick={handleBackButton}>
+                        <img src={leftArrowImage} width={25}/>
+                     </div>
+
+                     <GridItem item={toShow}/>
+
+                  </div>
+               }
+
+				</div>
+
+			</div>
+		</div>
+	)
+}
+
+export default App;
